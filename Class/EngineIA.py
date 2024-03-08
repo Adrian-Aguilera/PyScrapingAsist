ba60681a333facc75a1699843d0a7d546e4f6d8e
Antonio Pasasin <133169185+XclusivePasasin@users.noreply.github.com>
2024-03-07 20:43:14 -0600

    Create function UserAgent Ramdon in EngineIA and Fix Function get_UserAgentRamdon in Conexion.py

--- Class/EngineIA.py
+++ Class/EngineIA.py
@@ -10,19 +10,32 @@ class GetdataGPT:
             {"role": "system", "content": f"{self.Promt}"},
             {"role": "user", "content": html_content}
         ]
-
         stream = self.client.chat.completions.create(
             model=model,
             messages=messages,
             stream=True,
         )
-
         final_content = ""
         for chunk in stream:
             if chunk.choices[0].delta.content:
                 final_content += chunk.choices[0].delta.content
-
         return final_content.strip()
     
-    def UserAgent(self):
-        return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    def get_userAgent(self, model, PrototypeUserAgent):
+        messages = [
+            {"role": "system", "content": f"{PromtUserAgent}"},
+            {"role": "user", "content": PrototypeUserAgent}
+        ]
+        stream = self.client.chat.completions.create(
+            model=model,
+            messages=messages,
+            stream=True,
+        )
+        final_content = ""
+        for chunk in stream:
+            if chunk.choices[0].delta.content:
+                final_content += chunk.choices[0].delta.content
+        return final_content.strip()
+    
+    
+    
